d6bbfac8a3b2c68d1488069a530c3bd8561b6bf6
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-27 15:49:00 -0300

    Estrutura Sequencial - Seção 3

--- 1. Estrutura Sequencial/1. Estrutura Sequencial/Program.cs
+++ 1. Estrutura Sequencial/1. Estrutura Sequencial/Program.cs
@@ -1,30 +1,29 @@
 ﻿using System.Globalization;
 
-namespace uri1012
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-            double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+        double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
 
-            string[] valores = Console.ReadLine().Split(' ');
-            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
-            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
-            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+        Console.Write("Digite 3 números separados por espaço: ");
 
-            triangulo = A * C / 2.0;
-            circulo = 3.14159 * C * C;
-            trapezio = (A + B) / 2.0 * C;
-            quadrado = B * B;
-            retangulo = A * B;
+        string[] valores = Console.ReadLine().Split(' ');
+        A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+        B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+        C = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
-            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
-        }
+        triangulo = A * C / 2.0;
+        circulo = 3.14159 * C * C;
+        trapezio = (A + B) / 2.0 * C;
+        quadrado = B * B;
+        retangulo = A * B;
+
+        Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+        Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
     }
 }
