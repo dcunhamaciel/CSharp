1057f7d751dfaeef725081e766b40ef2241d085b
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-27 16:24:38 -0300

    Estrutura Condicional IF ELSE - Seção 3

---
+++ 2. Estrutura Condicional IF ELSE/2. Estrutura Condicional IF ELSE/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Digite um número: ");
+
+        int N = int.Parse(Console.ReadLine());
+
+        if (N % 2 == 0)
+        {
+            Console.WriteLine("PAR");
+        }
+        else
+        {
+            Console.WriteLine("IMPAR");
+        }
+    }
+}
