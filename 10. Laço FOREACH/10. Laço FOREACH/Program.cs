637bf9fc3ccb35fafa0afcf126b8be83c6cc5213
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-28 07:59:32 -0300

    Laço FOREACH - Seção 6

---
+++ 10. Laço FOREACH/10. Laço FOREACH/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] vect = new string[] { "Maria", "Bob", "Alex" };
+
+        foreach (string obj in vect)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
