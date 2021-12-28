e6e37567859a1c98764d1c295bf6e4f59a41b8e2
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-28 08:34:31 -0300

    Inferência VAR - Seção 7

---
+++ 13. Inferência VAR/13. Inferência VAR/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var x = 10;
+        var y = 20.0;
+        var z = "Maria";
+
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(z);
+    }
+}
