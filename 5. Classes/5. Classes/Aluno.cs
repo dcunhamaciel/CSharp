dac167761e5bb7c76ee8138d4f012f7af2c3855b
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-28 06:57:09 -0300

    Classes - Seção 4

---
+++ 5. Classes/5. Classes/Aluno.cs
@@ -0,0 +1,31 @@
+﻿class Aluno
+{
+    public string Nome;
+    public double Nota1, Nota2, Nota3;
+    public double NotaFinal()
+    {
+        return Nota1 + Nota2 + Nota3;
+    }
+    public bool Aprovado()
+    {
+        if (NotaFinal() >= 60.0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public double NotaRestante()
+    {
+        if (Aprovado())
+        {
+            return 0.0;
+        }
+        else
+        {
+            return 60.0 - NotaFinal();
+        }
+    }
+}
