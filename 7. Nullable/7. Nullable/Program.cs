9b19c59274a8102390cdd7ecf774a99b159ad7c2
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-28 07:34:36 -0300

    Nullable - Seção 6

---
+++ 7. Nullable/7. Nullable/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double? x = null;
+        double? y = 10.0;
+
+        Console.WriteLine(x.GetValueOrDefault());
+        Console.WriteLine(y.GetValueOrDefault());
+        Console.WriteLine(x.HasValue);
+        Console.WriteLine(y.HasValue);
+
+        if (x.HasValue)
+            Console.WriteLine(x.Value);
+        else
+            Console.WriteLine("X is null");
+        if (y.HasValue)
+            Console.WriteLine(y.Value);
+        else
+            Console.WriteLine("Y is null");
+
+        double? a = null;
+        double? b = 10.4;
+        
+        double c = a ?? 0.0;
+        double d = b ?? 0.0;
+
+        Console.WriteLine("c = " + c);
+        Console.WriteLine("d = " + d);
+    }
+}
