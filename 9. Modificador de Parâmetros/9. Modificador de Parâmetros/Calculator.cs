1b8d3e1d2c0c6a1abb1720e6ed9d37bd78c43a40
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-28 07:46:55 -0300

    Modificador de Parâmetros - Seção 6

---
+++ 9. Modificador de Parâmetros/9. Modificador de Parâmetros/Calculator.cs
@@ -0,0 +1,22 @@
+﻿class Calculator
+{
+    public static int Sum(params int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    public static void Triple(ref int x)
+    {
+        x = x * 3;
+    }
+
+    public static void Triple(int origin, out int result)
+    {
+        result = origin * 3;
+    }
+}
---
+++ 9. Modificador de Parâmetros/9. Modificador de Parâmetros/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Soma: " + Calculator.Sum(10, 20 , 30 ,40));
+
+        int a = 10;
+        Calculator.Triple(ref a);
+        Console.WriteLine("Triplo ref: " + a);
+
+        int triple;
+        Calculator.Triple(a, out triple);
+        Console.WriteLine("Triplo out: " + triple);
+    }
+}
