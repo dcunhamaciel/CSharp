190ac7d00121581b651653595159e6ffe90504ec
dcunhamaciel <dcunhamaciel@gmail.com>
2021-12-29 08:17:20 -0300

    Expressão Condicional Ternária - Seção 7

---
+++ 15. Expressão Condicional Ternária/15. Expressão Condicional Ternária/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        double preco = 34.5;
+        double desconto;
+
+        if (preco < 20.0)
+        {
+            desconto = preco * 0.1;
+        }
+        else
+        {
+            desconto = preco * 0.05;
+        }
+
+        double precoTernaria = 34.5;
+        double descontoTernaria = (precoTernaria < 20.0) ? precoTernaria * 0.1 : precoTernaria * 0.05;
+
+        Console.WriteLine("Preço: " + preco);
+        Console.WriteLine("Preço Ternária: " + precoTernaria);
+    }
+}
