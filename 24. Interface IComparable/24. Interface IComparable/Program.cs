016bbfa52b5c17c5e3bd6bb64d4356c79850d4d0
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-03 08:12:00 -0300

    Interface IComparable - Seção 14

---
+++ 24. Interface IComparable/24. Interface IComparable/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"D:\Cursos\CSharp\24. Interface IComparable\in.txt";
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                List<Employee> list = new List<Employee>();
+                while (!sr.EndOfStream)
+                {
+                    list.Add(new Employee(sr.ReadLine()));
+                }
+                list.Sort();
+                foreach (Employee emp in list)
+                {
+                    Console.WriteLine(emp);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
