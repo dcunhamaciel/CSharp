d7c46235bc7a5f8588f79acc74d1c33310966708
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-03 15:05:15 -0300

    HashSet - Seção 15

---
+++ 28. HashSet/28. HashSet/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        set.Add("TV");
+        set.Add("Notebook");
+        set.Add("Tablet");
+
+        Console.WriteLine(set.Contains("Notebook"));
+        Console.WriteLine();
+
+        foreach (String p in set)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
