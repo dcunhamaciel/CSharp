0241839a8ea52c0d6fbbe68ea699106c19eb2a85
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-03 13:19:48 -0300

    GetHashCode e Equals - Seção 15

---
+++ 27. GetHashCode e Equals/27. GetHashCode e Equals/Client.cs
@@ -0,0 +1,20 @@
+﻿public class Client
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is Client))
+        {
+            return false;
+        }
+        Client other = obj as Client;
+        return Email.Equals(other.Email);
+    }
+
+    public override int GetHashCode()
+    {
+        return Email.GetHashCode();
+    }
+}
---
+++ 27. GetHashCode e Equals/27. GetHashCode e Equals/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Client a = new Client { Name = "Alex", Email = "alex@gmail.com" };
+        Client b = new Client { Name = "Maria Flávia", Email = "maria@gmail.com" };
+        Client c = new Client { Name = "Maria Luiza", Email = "maria@gmail.com" };
+
+        Console.WriteLine("a.Equals(b) = " + a.Equals(b));
+        Console.WriteLine("b.Equals(c) = " + b.Equals(c));
+
+        Console.WriteLine();
+
+        Console.WriteLine("a.GetHashCode() = " + a.GetHashCode());
+        Console.WriteLine("b.GetHashCode() = " + b.GetHashCode());
+        Console.WriteLine("c.GetHashCode() = " + c.GetHashCode());
+    }
+}
