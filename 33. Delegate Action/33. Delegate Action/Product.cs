556ca5d6a9e73bbd060dc65108c7f00cd8dbae36
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-04 13:21:50 -0300

    Delegate Action - Seção 17

---
+++ 33. Delegate Action/33. Delegate Action/Product.cs
@@ -0,0 +1,16 @@
+﻿class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public override string ToString()
+    {
+        return Name + ", " + Price.ToString("F2");
+    }
+}
---
+++ 33. Delegate Action/33. Delegate Action/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<Product> list = new List<Product>();
+
+        list.Add(new Product("Tv", 900.00));
+        list.Add(new Product("Mouse", 50.00));
+        list.Add(new Product("Tablet", 350.50));
+        list.Add(new Product("HD Case", 80.90));
+
+        Action<Product> action = (p) => p.Price += p.Price * 0.1;
+        
+        list.ForEach(action);
+
+        foreach (Product p in list)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
