d60bd4e6b916401ca25fc8a920f405f00a7c00cd
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-03 21:17:39 -0300

    Extension Methods - Seção 16

---
+++ 31. Extension Methods/31. Extension Methods/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2021, 11, 16, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
+
+        String s1 = "Good morning dear students!";
+        Console.WriteLine(s1.Cut(10));
+    }
+}
---
+++ 31. Extension Methods/31. Extension Methods/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿static class StringExtensions
+{
+    public static string Cut(this string thisObj, int count)
+    {
+        if (thisObj.Length <= count)
+        {
+            return thisObj;
+        }
+        else
+        {
+            return thisObj.Substring(0, count) + "...";
+        }
+    }
+}
