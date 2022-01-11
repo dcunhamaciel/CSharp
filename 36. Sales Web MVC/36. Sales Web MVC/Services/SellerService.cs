e932d7bc0c1f18b6ebfe39caa3c31c675cbf2135
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-11 20:07:15 -0300

    Delete Seller

--- 36. Sales Web MVC/36. Sales Web MVC/Services/SellerService.cs
+++ 36. Sales Web MVC/36. Sales Web MVC/Services/SellerService.cs
@@ -17,9 +17,21 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById(int id)
+        {            
+            return _context.Seller.FirstOrDefault(seller => seller.Id == id);
+        }
+
         public void Insert(Seller seller)
         {
             _context.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            Seller seller = FindById(id);
+            _context.Remove(seller);
             _context.SaveChanges();
         }
     }
